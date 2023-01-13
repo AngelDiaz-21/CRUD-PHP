c3a7832a3b6ad3e1aabc239911aab50afba98438
Angel <angelSistemas21@gmail.com>
2023-01-13 11:48:17 -0600

    add: View main

---
+++ views/main/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PDO</title>
+	<link rel="stylesheet" type="text/css" href="librerias/bootstrap5/bootstrap.min.css">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="stylesheet" href="public/style.css">
+</head>
+<body>
+
+	<header>
+		<?php
+			require 'views/header.php';
+		?>
+	</header>
+
+    <main>
+		<section class="d-flex justify-content-center mt-5">
+			<h1>Sección principal</h1>
+		</section>
+	</main>
+
+	<footer>
+		<?php
+			require 'views/footer.php';
+		?>
+	</footer>
+
+
+	<!-- <script src="librerias/bootstrap5/jquery-3.4.1.min.js"></script>
+	<script src="librerias/bootstrap5/popper.min.js"></script>
+	<script src="librerias/bootstrap5/bootstrap.min.js"></script>
+	<script src="librerias/sweetalert.min.js"></script>
+	<script src="js/crud.js"></script>
+
+
+	<script type="text/javascript">
+		mostrar();
+	</script> -->
+
+</body>
+</html>
