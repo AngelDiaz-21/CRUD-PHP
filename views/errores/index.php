96e4546862095529a587fb716fb7bc12cee3f6bf
Angel <angelSistemas21@gmail.com>
2023-01-13 02:25:19 -0600

    add: view 404

---
+++ views/errores/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PDO</title>
+    <link rel="stylesheet" type="text/css" href="librerias/bootstrap5/bootstrap.min.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+</head>
+<body>
+<div class="d-flex align-items-center justify-content-center vh-100">
+            <div class="text-center">
+                <h1 class="display-1 fw-bold">404</h1>
+                <p class="fs-3"> <span class="text-danger">Opps!</span> Page not found.</p>
+                <p class="lead">
+                    The page you’re looking for doesn’t exist.
+                </p>
+                <a href="<?php echo constant('URL');?>" class="btn btn-primary">Go Home</a>
+            </div>
+        </div>
+    <script src="librerias/bootstrap5/jquery-3.4.1.min.js"></script>
+    <script src="librerias/bootstrap5/popper.min.js"></script>
+    <script src="librerias/bootstrap5/bootstrap.min.js"></script>
+    <script src="librerias/sweetalert.min.js"></script>
+
+</body>
+</html>
