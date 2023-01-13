d29decca2cb7ab0f91c864c59093b45a7a71d91e
Angel <angelSistemas21@gmail.com>
2023-01-13 11:47:20 -0600

    add: Sections header and footer

---
+++ views/footer.php
@@ -0,0 +1,3 @@
+<div class="bg-dark text-white d-flex justify-content-center py-3 fw-bold">
+    <span class="h-100">MVC 2023</span>
+</div>
---
+++ views/header.php
@@ -0,0 +1,12 @@
+<nav class="d-flex justify-content-between align-items-center p-3 bg-dark text-white">
+    <a class="text-white"  href="<?php echo constant('URL');?>">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/PHP-logo.svg/120px-PHP-logo.svg.png" alt="php"/>
+    </a>
+    <ul>
+        <li class="d-flex">
+            <a class="text-decoration-none text-white me-5" href="<?php echo constant('URL');?>">Inicio</a>
+            <a class="text-decoration-none text-white me-5" href="">Alumnos</a>
+            <a class="text-decoration-none text-white me-3" href="">Empleados</a>
+        </li>
+    </ul>
+</nav>
