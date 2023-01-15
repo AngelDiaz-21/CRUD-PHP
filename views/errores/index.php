ed74bd72205b85bd7dc7b2f5f7baa4de76fb96d3
Angel <angelSistemas21@gmail.com>
2023-01-14 22:12:12 -0600

    feat: Message from controller-errors

--- views/errores/index.php
+++ views/errores/index.php
@@ -8,16 +8,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 </head>
 <body>
-<div class="d-flex align-items-center justify-content-center vh-100">
-            <div class="text-center">
-                <h1 class="display-1 fw-bold">404</h1>
-                <p class="fs-3"> <span class="text-danger">Opps!</span> Page not found.</p>
-                <p class="lead">
-                    The page you’re looking for doesn’t exist.
-                </p>
-                <a href="<?php echo constant('URL');?>" class="btn btn-primary">Go Home</a>
-            </div>
+    <div class="d-flex align-items-center justify-content-center vh-100">
+        <div class="text-center">
+            <h1 class="display-1 fw-bold">404</h1>
+            <p class="fs-3"> <span class="text-danger">Opps!</span> Page not found.</p>
+            <p class="lead">
+                <?php
+                    echo $this->mensaje; 
+                ?>
+            </p>
+            <a href="<?php echo constant('URL');?>" class="btn btn-primary">Go Home</a>
         </div>
+    </div>
     <script src="librerias/bootstrap5/jquery-3.4.1.min.js"></script>
     <script src="librerias/bootstrap5/popper.min.js"></script>
     <script src="librerias/bootstrap5/bootstrap.min.js"></script>
