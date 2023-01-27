2c5847c9665f63c0736fa691543d8ade93137d83
Angel <angelSistemas21@gmail.com>
2023-01-26 23:57:57 -0600

    fix: Bootstrap link

--- views/errores/index.php
+++ views/errores/index.php
@@ -2,10 +2,11 @@
 <html>
 <head>
     <title>PDO</title>
-    <link rel="stylesheet" type="text/css" href="librerias/bootstrap5/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="../../librerias/bootstrap5/bootstrap.min.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="icon" href="https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/PHP-logo.svg/120px-PHP-logo.svg.png" sizes="32x32">
 </head>
 <body>
     <div class="d-flex align-items-center justify-content-center vh-100">
@@ -24,6 +25,5 @@
     <script src="librerias/bootstrap5/popper.min.js"></script>
     <script src="librerias/bootstrap5/bootstrap.min.js"></script>
     <script src="librerias/sweetalert.min.js"></script>
-
 </body>
 </html>
